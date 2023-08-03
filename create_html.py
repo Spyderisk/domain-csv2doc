b7d459f60fcd618df7ff2a1c0f583198dfcd3849
Josh Tucker <joshuatucker648@gmail.com>
2023-08-03 14:12:18 +0100

    Added icons to asset pages

--- create_html.py
+++ create_html.py
@@ -335,13 +335,13 @@ def see_asset(uri):
     # Get lines from file
     lines = get_lines(file_path, False)
 
-    # Get misbehaviour and assets
-    # for line in lines[0:-1]:
-    #     if line.startswith('Misbehaviour:'):
-    #         misbehaviour = line.split(':')[1]
+    # Get asset icon
+    for line in lines[0:-1]:
+        if line.startswith('Icon:'):
+            icon = line.split(':')[1]
 
     return render_template('pattern/asset.html', uri=uri, descriptions=descriptions, labels=labels,
-                           package=packages[uri], search_index=json.dumps(search_index),
+                           package=packages[uri], icon=icon, search_index=json.dumps(search_index),
                            model_version=model_version, prev_uri=proceeded_by['asset'][uri],
                            next_uri=followed_by['asset'][uri])
 
